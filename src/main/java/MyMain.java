6705365cd20169aba9d6f355783fa047c3895cf7
adrianartale <23aartale@athenian.org>
2021-09-08 14:19:43 -0700

    attempt 1

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -8,56 +8,63 @@ public class MyMain {
     // scan.nextDouble() might be useful here
     public static double inputDouble() {
         Scanner scan = new Scanner(System.in);
-        // REPLACE THIS WITH YOUR CODE
-
-        return -1.0;
+        while (true) {
+            System.out.println("Type in a number");
+            Double input = scan.nextDouble();
+            if (input <= 0 || input > 1000000) {
+            } else {
+                System.out.println("Nice!");
+                return input;
+            }
+        }
     }
 
     // Takes a double money as input and returns the number of quarters that we
     // can return in change
     // You should use a while loop!
     public static int numQuarters(double money) {
-        // REPLACE THIS WITH YOUR CODE
-        return 0;
+        int quarters = 0;
+        while (money >= 0.25) {
+            money = money - 0.25;
+            quarters = quarters + 0;
+            return quarters;
+        }
+
+
+        // Takes a double money as input and returns the number of dimes that we
+        // can return in change
+        // You should use a while loop!
+        public static int numDimes ( double money){
+            // REPLACE THIS WITH YOUR CODE
+            return 0;
+        }
+
+
+        //  Takes in two Strings as input and returns a String equal to those two Strings added
+        //  together in alphabetic order.
+        public static String stringOrder (String str1, String str2){
+            return "";
+        }
+
+
+        public static void main (String[]args){
+            Scanner scan = new Scanner(System.in);
+
+            // You should test inputDouble yourself!
+            double d = inputDouble();
+
+            // Some code to test numQuarters
+            System.out.println("\nnumQuarters tests:");
+            System.out.println("1.25 in quarters would be: " + numQuarters(1.25) + " quarters."); // 5
+            System.out.println("10.55 in quarters would be: " + numQuarters(10.55) + " quarters."); // 42
+            System.out.println("1000000.20 in quarters would be: " + numQuarters(1000000.20) + " quarters."); //4000000
+
+            // Some code to test numDimes
+            System.out.println("\nnumDimes tests:");
+            System.out.println("1.20 in dimes would be: " + numDimes(1.20) + " dimes."); // 12
+            System.out.println("120.13 in dimes would be: " + numDimes(120.13) + " dimes."); //1201
+            System.out.println("150.25 in dimes would be: " + numDimes(150.25) + " dimes."); //1502
+
+            scan.close();
+        }
     }
-
-    // Takes a double money as input and returns the number of dimes that we
-    // can return in change
-    // You should use a while loop!
-    public static int numDimes(double money) {
-        // REPLACE THIS WITH YOUR CODE
-        return 0;
-    }
-
-
-
-
-
-    //  Takes in two Strings as input and returns a String equal to those two Strings added
-    //  together in alphabetic order.
-    public static String stringOrder(String str1, String str2) {
-        return "";
-    }
-
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        // You should test inputDouble yourself!
-        double d = inputDouble();
-
-        // Some code to test numQuarters
-        System.out.println("\nnumQuarters tests:");
-        System.out.println("1.25 in quarters would be: " + numQuarters(1.25) + " quarters."); // 5
-        System.out.println("10.55 in quarters would be: " + numQuarters(10.55) + " quarters."); // 42
-        System.out.println("1000000.20 in quarters would be: " + numQuarters(1000000.20) + " quarters."); //4000000
-
-        // Some code to test numDimes
-        System.out.println("\nnumDimes tests:");
-        System.out.println("1.20 in dimes would be: " + numDimes(1.20) + " dimes."); // 12
-        System.out.println("120.13 in dimes would be: " + numDimes(120.13) + " dimes."); //1201
-        System.out.println("150.25 in dimes would be: " + numDimes(150.25) + " dimes."); //1502
-
-        scan.close();
-    }
-}
